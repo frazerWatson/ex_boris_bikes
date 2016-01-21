22236ca9a2ac7963037d8d735b3fd5fc7c6383a9
fareedpatel <fareed_patel@hotmail.com>
2016-01-21 09:29:41 +0000

    Chapter 9

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,3 +2,11 @@ require "docking_station"
 describe DockingStation  do
 	it { is_expected.to respond_to :release_bike }
 end
+
+require "bike"
+describe Bike do
+	it 'gets bike' do
+	bike = Bike.new
+	bike.working? 'gets bike'
+	end
+end
