ad4f62050dc5585c0d701690af9b2ed1d340d5c1
fareedpatel <fareed_patel@hotmail.com>
2016-01-21 14:58:53 +0000

    Challenge-10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,21 @@
-require "docking_station"
-describe DockingStation  do
-	it { is_expected.to respond_to :release_bike }
-end
+require 'docking_station'
 
-require "bike"
-describe Bike do
-	it 'gets bike' do
-	bike = Bike.new
-	bike.working? 'gets bike'
+	describe DockingStation do
+	
+		it { is_expected.to respond_to :release_bike }
+
+		it 'releases working bikes' do
+		bike = subject.release_bike
+		expect(bike).to be_working
 	end
-end
+
+	end
+
+
+
+# require "bike"
+# describe Bike do
+# 	it 'gets bike' do
+# 	bike = Bike.new
+# 	bike.working? 'gets bike'
+# 	ene
